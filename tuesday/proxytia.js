954c5c8909cb02e54e3bb12b4c5734cd9e473dd8
Zoe <zoezachary27@gmail.com>
2019-09-25 23:40:12 +0700

    uploading proxytia.js

---
+++ tuesday/proxytia.js
@@ -0,0 +1,36 @@
+function printSelamatDatang(nama) {
+  console.log('Selamat datang di Dunia Proxytia, ' + nama);
+}
+
+function cekValiditasPeran(peran) {
+  console.log('Peran yang di cek: ' + peran);
+
+  if(peran == '' || peran == null) return false;
+
+  if(peran.toLowerCase() == 'ksatria' || peran.toLowerCase() == 'tabib' || peran.toLowerCase() == 'penyihir') {
+    return true;
+  } else return false;
+}
+
+var nama = prompt('masukan nama');
+
+while(nama == '' || nama == null) {
+  nama = prompt('Nama harus diisi!');
+}
+
+var peran = prompt('Halo ' + nama + ', Pilih peranmu untuk memulai game! (Ksatria / Tabib / Penyihir)');
+
+while (!cekValiditasPeran(peran)) {
+  peran = prompt('Halo ' + nama + ', Pilih peranmu untuk memulai game! (Ksatria / Tabib / Penyihir)');
+}
+
+if(peran == 'ksatria') {
+  printSelamatDatang(nama);
+  console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else if (peran == 'tabib') {
+  printSelamatDatang(nama);
+  console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
+} else {
+  printSelamatDatang(nama);
+  console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+}
