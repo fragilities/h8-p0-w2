c0c94d8f9211501c6772355a1eb2099336c51091
Zoe <zoezachary27@gmail.com>
2019-09-26 16:16:13 +0700

    uploading looping-asterisks.js | all tasks completed.

---
+++ tuesday/looping-asterisks.js
@@ -0,0 +1,31 @@
+// Hacktiv8 phase 0 week 2
+// Tugas hari selasa: Perulangan - Play with Asterisks
+// Zoe Zachary
+
+// 1. Menyusun Barisan Bintang
+
+var rows1 = 5;
+
+for(var i = 0; i < rows1; i++) {
+  console.log('*');
+}
+
+// 2. Menyusun Barisan Bintang Dengan Nested Looping
+
+var rows2 = 5;
+var temp = '';
+for (var i = 0; i < rows2; i++) {
+  for(var j = 0; j < rows2; j++) temp = temp + '*';
+  console.log(temp);
+  temp = '';
+}
+
+// 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+var rows3 = 5;
+var temp = '';
+
+for(var i = 0; i < rows3; i++) {
+  for(var j = 0; j <= i; j++) temp = temp + '*';
+  console.log(temp);
+  temp = '';
+}
--- tuesday/looping.js
+++ tuesday/looping.js
@@ -22,33 +22,33 @@ while(backwardCount >= 2) {
 // 2. Melakukan Looping Menggunakan For
 
 console.log('LOOPING PERTAMA');
-for(i = 1; i <= 20; i++) {
+for(var i = 1; i <= 20; i++) {
   console.log(i + ' - I love coding');
 }
 
 console.log('LOOPING KEDUA');
-for(i = 20; i >= 1; i--) {
+for(var i = 20; i >= 1; i--) {
   console.log(i + ' - I will become fullstack developer');
 }
 
 // 3. Angka Ganjil dan Genap
 
-for(i = 1; i <= 100; i++) {
+for(var i = 1; i <= 100; i++) {
   if(i%2 != 0) console.log('GANJIL');
   else console.log('GENAP');
 }
 
 var increament = 2;
-for(i = 1; i <= 100; i += increament) {
+for(var i = 1; i <= 100; i += increament) {
   if(i%3 == 0) console.log(i + ' kelipatan 3');
 }
 
 increament = 5;
-for(i = 1; i <= 100; i += increament) {
+for(var i = 1; i <= 100; i += increament) {
   if(i%6 == 0) console.log(i + ' kelipatan 6');
 }
 
 increament = 9;
-for(i = 1; i <= 100; i += increament) {
+for(var i = 1; i <= 100; i += increament) {
   if(i%10 == 0) console.log(i + ' kelipatan 10');
 }
