861750af21a83f8780ad4cbae3fcd4a0a5e559d2
Zoe <zoezachary27@gmail.com>
2019-09-26 16:46:16 +0700

    uploading reverseString.js | task completed.

---
+++ tuesday/reverseString.js
@@ -0,0 +1,20 @@
+// Hacktiv8 phase 0 week 2
+// Tugas hari Jumat: Balik Kata
+// Zoe Zachary
+
+function balikKata(kata) {
+  // you can only write your code here!
+  var temp = '';
+  for(var i = kata.length-1; i >= 0; i--) {
+    temp = temp + kata[i];
+  }
+
+  return temp;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
